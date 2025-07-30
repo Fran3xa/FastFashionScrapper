095a6d36225961c10cfe9f0fc5a0fbf9baabc507
Fran3xa <fran3xa@gmail.com>
2025-07-30 20:33:05 +0200

    Codigo limpio

--- backendJava/src/main/java/com/Fran3xa/FastFashionRecap/model/dao/IProductDao.java
+++ backendJava/src/main/java/com/Fran3xa/FastFashionRecap/model/dao/IProductDao.java
@@ -1,9 +1,14 @@
 package com.Fran3xa.FastFashionRecap.model.dao;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.Fran3xa.FastFashionRecap.model.entitys.Product;
 
 public interface IProductDao extends CrudRepository<Product, Long> {
+
+    List<Product> findByGeneroAndLanguageAndMarca(String genero, String language, String marca);
+
 
 }
--- scraperPython/domain/product.py
+++ scraperPython/domain/product.py
@@ -1,33 +1,43 @@
 class Product:
-    def __init__(self, productId, ref, nombre, precioNodisc, precioDisc, discount, imageUrl):  
+    def __init__(self, productId, ref, genero, nombre, precioNodisc, precioDisc, discount, imageUrl, marca, language, color=None, categoria=None, component_ids=None):
+        """
+        Inicializa un producto.
+        Args:
+            component_ids (list, optional): Lista de IDs de los productos que componen un bundle. Defaults to None.
+        """
         self.productId = productId
         self.ref = ref
+        self.genero = genero
         self.nombre = nombre
         self.precioNodisc = precioNodisc
         self.precioDisc = precioDisc
         self.discount = discount
         self.imageUrl = imageUrl
-        self.categoria = None
-        self.subcategoria = None
-        self.color = None  # Añade el campo si vas a usarlo luego
+        self.component_ids = component_ids if component_ids is not None else []
+        self.marca=marca
+        self.categoria = categoria
+        self.color = color
+        self.language = language
 
     def to_dict(self):
         return self.__dict__
 
     @classmethod
     def from_dict(cls, data):
-        # Extrae solo los argumentos del constructor
+    
         product = cls(
             productId=data['productId'],
             ref=data['ref'],
+            genero=data['genero'],
             nombre=data['nombre'],
             precioNodisc=data['precioNodisc'],
             precioDisc=data['precioDisc'],
             discount=data['discount'],
-            imageUrl=data['imageUrl']
+            imageUrl=data['imageUrl'],
+            marca=data['marca'],
+            component_ids=data.get('component_ids'), 
+            categoria=data['categoria'],
+            color=data['color'],
+            language=data['language']
         )
-        # Asigna los atributos opcionales
-        product.categoria = data.get('categoria')
-        product.subcategoria = data.get('subcategoria')
-        product.color = data.get('color')
         return product
